05b7e42d92d162566652f386d3bfd89bbf0f7129
João Paulo <pmagal539@gmail.com>
2024-06-03 13:57:54 -0300

    implementando metodo de imprimir tabuleiro

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using jogo_xadrez;
 using tabuleiro;
 
 namespace xadrez_console {
@@ -7,6 +8,7 @@ namespace xadrez_console {
         static void Main( string[] args ) {
 
             Tabuleiro tabuleiro = new Tabuleiro(8,8);
+            Tela.imprimirTabuleiro(tabuleiro);
         }
     }
 }
