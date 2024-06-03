7f7ce29477af53a30917f93ad3e7cfdc2a295223
João Paulo <pmagal539@gmail.com>
2024-06-03 14:35:45 -0300

    Comecando a colocar pecas no tabuleiro

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -2,12 +2,18 @@
 
 using jogo_xadrez;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console {
     class Program {
         static void Main( string[] args ) {
 
             Tabuleiro tabuleiro = new Tabuleiro(8,8);
+
+            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta), new Posicao(0,0));
+            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1,3));
+            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2,4));
+
             Tela.imprimirTabuleiro(tabuleiro);
         }
     }
