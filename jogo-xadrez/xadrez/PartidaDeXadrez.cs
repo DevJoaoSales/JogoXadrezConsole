01ddeac15729f11ebd78149c478bfd28dc1ef75a
João Paulo <pmagal539@gmail.com>
2024-06-13 17:48:33 -0300

    Jogada especial promocao

--- jogo-xadrez/xadrez/PartidaDeXadrez.cs
+++ jogo-xadrez/xadrez/PartidaDeXadrez.cs
@@ -128,6 +128,19 @@ internal class PartidaDeXadrez {
             throw new TabuleiroException("Você não pode se colocar em Xeque!");
         }
 
+        Peca p = tab.peca(destino);
+
+        // #jogadaespecial promoção.
+        if ( p is Peao ) {
+            if ( ( p.cor == Cor.Branca && destino.linha == 0 ) || ( p.cor == Cor.Preta && destino.linha == 7 ) ) {
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(tab, p.cor);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama);
+            }
+        }
+
         if (estaEmXeque(adversaria(JogadorAtual))) {
             xeque = true;
         } else {
@@ -140,8 +153,6 @@ internal class PartidaDeXadrez {
             turno++;
             mudarJogador();
         }
-
-        Peca p = tab.peca(destino);
 
         // #jogadaespecial en passant.
         if ( p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2) ) {
@@ -267,12 +278,12 @@ internal class PartidaDeXadrez {
 
     private void colocarPecas() {
         colocarNovaPeca('a', 1, new Torre(tab, Cor.Branca));
-        //colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
-        //colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
-        //colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
+        colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
+        colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
+        colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
         colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
-        //colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
-        //colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
+        colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
+        colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
         colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
 
         colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca, this));
@@ -281,21 +292,21 @@ internal class PartidaDeXadrez {
         colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca, this));
         colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca, this));
         colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca, this));
-        colocarNovaPeca('g', 5, new Peao(tab, Cor.Branca, this));
+        colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca, this));
         colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca, this));
 
 
         colocarNovaPeca('a', 8, new Torre(tab, Cor.Preta));
-        //colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
-        //colocarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
-        //colocarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
+        colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
+        colocarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
+        colocarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
         colocarNovaPeca('e', 8, new Rei(tab, Cor.Preta, this));
-        //colocarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
-        //colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
+        colocarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
+        colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
         colocarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
 
         colocarNovaPeca('a', 7, new Peao(tab, Cor.Preta, this));
-        colocarNovaPeca('b', 4, new Peao(tab, Cor.Preta, this));
+        colocarNovaPeca('b', 7, new Peao(tab, Cor.Preta, this));
         colocarNovaPeca('c', 7, new Peao(tab, Cor.Preta, this));
         colocarNovaPeca('d', 7, new Peao(tab, Cor.Preta, this));
         colocarNovaPeca('e', 7, new Peao(tab, Cor.Preta, this));
